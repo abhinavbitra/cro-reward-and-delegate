a3c932b1028422e129e4c712f2ecf3efb6c87a50
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-19 10:28:53 -0600

    Update reward-and-delegate-nokey.sh

--- reward-and-delegate-nokey.sh
+++ reward-and-delegate-nokey.sh
@@ -23,12 +23,12 @@ do
     if (( $(echo "$currentAvailableReward > 10000000000" |bc -l) )) 
     then
             echo "Withdrawing rewards..."
-            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
     if (( $(echo "$currentBalance > 10000000000" |bc -l) )) 
     then
             echo "Re-delegating rewards..."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress "$currentBalance"basetcro --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress "$currentBalance"tcro --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
     sleep 1m
 done
