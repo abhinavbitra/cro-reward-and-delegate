b412793ffb449757e7163e18d38c24fd5a610a80
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-19 11:00:43 -0600

    Update reward-and-delegate-nokey.sh

--- reward-and-delegate-nokey.sh
+++ reward-and-delegate-nokey.sh
@@ -19,15 +19,15 @@ do
     echo "Current balance: $currentBalance"
     currentAvailableReward=`./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
     echo "Current Available Delegator Rewards: $currentAvailableReward"
-    if (( $(echo "$currentAvailableReward > 10000000000" |bc -l) )) 
+    if (( $(echo "$currentAvailableReward > 100000000" |bc -l) )) 
     then
             echo "Withdrawing rewards...."
             echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
-    if (( $(echo "$currentBalance > 10000000000" |bc -l) )) 
+    if (( $(echo "$currentBalance > 100000000" |bc -l) )) 
     then
             echo "Re-delegating rewards...."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress "$currentBalance"tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 1tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
     sleep 1m
 done
