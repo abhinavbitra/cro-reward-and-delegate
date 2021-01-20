7f3554cfbab8a2ad0ddb779c1539ad0de4576256
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-20 08:12:11 -0600

    Create reward-and-delegate-nokey-p.sh

---
+++ reward-and-delegate-nokey-p.sh
@@ -0,0 +1,33 @@
+#!/bin/bash --
+
+echo "Crypto.com Automatic Validator Operations inspired by Christian Vari"
+
+if [ "$#" == 0 ]
+then
+    echo "Please run the script as:"
+    echo "./reward-and-delegate-nokey.sh <operatorAddress> <validatorAddress> <keyPassword> <node>"
+    exit 0
+fi
+
+operatorAddress=$1
+validatorAddress=$2
+keyPassword=$3
+node=$4
+while [ true ]
+do
+    currentBalance=`./chain-maind query bank balances $operatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
+    echo "Current balance: $currentBalance"
+    currentAvailableReward=`./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
+    echo "Current Available Delegator Rewards: $currentAvailableReward"
+    if (( $(echo "$currentAvailableReward > 100000000" |bc -l) )) 
+    then
+            echo "Withdrawing rewards...."
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+    fi
+    if (( $(echo "$currentBalance > 100000000" |bc -l) )) 
+    then
+            echo "Re-delegating rewards...."
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 1tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+    fi
+    sleep 5s
+done
