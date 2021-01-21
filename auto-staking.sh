e6f7f54c3e399e2bbc9cb66bffd9cd776e88d6f4
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-21 17:05:57 -0600

    Update auto-staking.sh

--- auto-staking.sh
+++ auto-staking.sh
@@ -15,6 +15,13 @@ keyPassword=$3
 node=$4
 while [ true ]
 do
+    currentBalance=`./chain-maind query bank balances $operatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
+    echo "Current balance: $currentBalance"
+    currentAvailableReward=`./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
+    echo "Current Available Delegator Rewards: $currentAvailableReward"
+    fi
+    if (( $(echo "$currentBalance > 100000000" |bc -l) )) 
+    then
             echo "Re-delegating rewards...."
             echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 1tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
