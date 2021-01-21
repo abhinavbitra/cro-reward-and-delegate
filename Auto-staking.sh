0be4393bd1776694490c00fc22620780e2db595e
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-21 16:57:49 -0600

    Create Auto-staking.sh

---
+++ Auto-staking.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --
+
+echo "Crypto.com Automatic staker abhi"
+
+if [ "$#" == 0 ]
+then
+    echo "Please run the script as:"
+    echo "./reward-and-delegate-nokey.sh <operatorAddress> <validatorAddress> <keyPassword> <node>"
+    exit 0
+fi
+
+operatorAddress=$1
+validatorAddress=$2
+keyPassword=$3
+node=$4
+while [ true ]
+do
+            echo "Re-delegating rewards...."
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 1tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+    fi
+    sleep 0s
+done
