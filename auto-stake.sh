76e38bbd135fa942d3672e29ab19b1cfb1a0cb01
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-24 15:37:31 -0600

    Update auto-stake.sh

--- auto-stake.sh
+++ auto-stake.sh
@@ -11,8 +11,8 @@ KEYNAME=cross-fire-testing
 PASSPHRASE=qwertyabcd
 OPERATOR=crocncl1k7yvmaffyp8nnp7xepcx0rashu8rv3yuk30923
 CHAINID=crossfire
-COUNT=100 #Number of transactions till check of last transaction
-SLEEP=20 #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
+COUNT=50 #Number of transactions till check of last transaction
+SLEEP=5 #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
 CHECKTIME=2s #time between retries for check of last transaction
 SHOWTX=count #show tx-hashes in the output [true|new|count|count+new|point|false]
 VARBEGIN=true #show all variables on startup
