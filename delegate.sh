dc28db73406f9f40f2ffbf23d0f51f45a477989f
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-24 19:03:28 -0600

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -9,7 +9,7 @@ CHAINID=crossfire
 echo $PASSPHRASE | ./chain-maind tx distribution withdraw-rewards $OPERATOR --from $KEYNAME --chain-id "CHAINID" --gas 800000 --gas-prices="0.1basetcro" --commission --yes
 
 
-AMOUNT=$(/home/eric/chain-maind q bank balances $ADDRESS | grep amount | cut -d " " -f3|sed 's/"//g')
+AMOUNT=$(./chain-maind q bank balances $ADDRESS | grep amount | cut -d " " -f3|sed 's/"//g')
 CRO=$(( AMOUNT / 100000000 ))
 printf "Balance: $CRO tCRO"
 echo $AMOUNT
