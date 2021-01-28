853db8559302073403f089769ebfdadefaaee089
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 14:05:16 -0600

    Update auto-stake.sh

--- auto-stake.sh
+++ auto-stake.sh
@@ -12,8 +12,8 @@ PASSPHRASE=qwertyabcd
 OPERATOR=crocncl1k7yvmaffyp8nnp7xepcx0rashu8rv3yuk30923 # [crocncl1.....]
 CHAINID=crossfire
 TENDERMINT=https://crossfire.crypto.com/
-COUNT=200 #Number of transactions till check of last transaction
-SLEEP=40s #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
+COUNT=500 #Number of transactions till check of last transaction
+SLEEP=60s #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
 CHECKTIME=10s #time between retries for check of last transaction
 SHOWTX=count+new #show tx-hashes in the output [true|new|count|point|false]
 VARBEGIN=true #show all variables on startup
@@ -30,7 +30,7 @@ clear
 
 printf "\n\e[35m'Automated transaction creator' by eric" #(https://discord.com/channels/783264383978569728/790404424433926155/801438774000091208)
 
-sleep 5s
+sleep 2s
 
 if [ $VARBEGIN = true ]
 then
