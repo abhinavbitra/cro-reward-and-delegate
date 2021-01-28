f033030f7eb5af72f77d246adab5b18a9a5f5cd8
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 17:51:09 -0600

    Update reward-and-delegate-nokey.sh

--- reward-and-delegate-nokey.sh
+++ reward-and-delegate-nokey.sh
@@ -5,7 +5,7 @@ echo "Crypto.com Automatic Validator Operations script by Christian Vari"
 if [ "$#" == 0 ]
 then
     echo "Please run the script as:"
-    echo "./reward-and-delegate-nokey.sh <operatorAddress> <validatorAddress> <keyPassword> <node>"
+    echo "./automatic_validator_operations.sh <operatorAddress> <validatorAddress> <keyPassword> <node>"
     exit 0
 fi
 
@@ -19,15 +19,15 @@ do
     echo "Current balance: $currentBalance"
     currentAvailableReward=`./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
     echo "Current Available Delegator Rewards: $currentAvailableReward"
-    if (( $(echo "$currentAvailableReward > 100000000" |bc -l) )) 
+    if (( $(echo "$currentAvailableReward > 10000000000" |bc -l) )) 
     then
-            echo "Withdrawing rewards...."
-            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+            echo "Withdrawing rewards..."
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
     fi
-    if (( $(echo "$currentBalance > 100000000" |bc -l) )) 
+    if (( $(echo "$currentBalance > 10000000000" |bc -l) )) 
     then
-            echo "Re-delegating rewards...."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.0001tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+            echo "Re-delegating rewards..."
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress "0.001"basetcro --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
     fi
-    sleep 5s
+    sleep 5m
 done
