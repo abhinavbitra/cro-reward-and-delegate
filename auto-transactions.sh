a1ec5d838738b69f5aefbb4353ac1b37fae80d93
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 18:45:06 -0600

    Update auto-transactions.sh

--- auto-transactions.sh
+++ auto-transactions.sh
@@ -11,8 +11,8 @@ KEYNAME=cross-fire-testing
 PASSPHRASE=qwertyabcd
 OPERATOR=crocncl1k7yvmaffyp8nnp7xepcx0rashu8rv3yuk30923
 CHAINID=crossfire
-COUNT=500 #Number of transactions till check of last transaction
-SLEEP=60s #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
+COUNT=200 #Number of transactions till check of last transaction
+SLEEP=10s #length of the sleep before the scrip tries to check if the last transaction was broadcasted (0 = disabled)
 CHECKTIME=10s #time between retries for check of last transaction
 SHOWTX=count #show tx-hashes in the output [true|new|count|count+new|point|false]
 VARBEGIN=true #show all variables on startup
