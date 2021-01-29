8329901bcd657cc8cbfcbf9af0c8808e9efc1f30
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 20:42:10 -0600

    Update reward-and-delegate-nokey-p.sh

--- reward-and-delegate-nokey-p.sh
+++ reward-and-delegate-nokey-p.sh
@@ -12,6 +12,7 @@ fi
 operatorAddress=$1
 validatorAddress=$2
 keyPassword=$3
+node=$4
 while [ true ]
 do
     currentBalance=`./chain-maind query bank balances $operatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
@@ -21,12 +22,12 @@ do
     if (( $(echo "$currentAvailableReward > 10000" |bc -l) )) 
     then
             echo "Withdrawing rewards...."
-            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node "https://crossfire.crypto.com:443"  -y
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
     if (( $(echo "$currentBalance > 10000" |bc -l) )) 
     then
             echo "Re-delegating rewards...."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.001tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node "https://crossfire.crypto.com:443"  -y
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.001tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
     fi
     sleep 2s
 done
