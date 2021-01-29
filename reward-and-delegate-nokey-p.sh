f36b2037792586790e7ce45202b717940cae1030
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 21:24:28 -0600

    Update reward-and-delegate-nokey-p.sh

--- reward-and-delegate-nokey-p.sh
+++ reward-and-delegate-nokey-p.sh
@@ -1,11 +1,11 @@
 #!/bin/bash --
 
-echo "Crypto.com Automatic Validator Operations"
+echo "Crypto.com Automatic Validator Operations script by Christian Vari"
 
 if [ "$#" == 0 ]
 then
     echo "Please run the script as:"
-    echo "./reward-and-delegate-nokey-p.sh <operatorAddress> <validatorAddress> <keyPassword> <node>"
+    echo "./automatic_validator_operations.sh <operatorAddress> <validatorAddress> <keyPassword> <keyring> <node>"
     exit 0
 fi
 
@@ -15,14 +15,19 @@ keyPassword=$3
 node=$4
 while [ true ]
 do
-    currentBalance=./chain-maind query bank balances $operatorAddress --output=json --node $node | jq -r ".balances[0].amount"
+    currentBalance=`./chain-maind query bank balances $operatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
     echo "Current balance: $currentBalance"
-    currentAvailableReward=./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"
+    currentAvailableReward=`./chain-maind query distribution rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
     echo "Current Available Delegator Rewards: $currentAvailableReward"
-    if (( $(echo "$currentBalance > 10000" |bc -l) )) 
+    if (( $(echo "$currentAvailableReward > 1000" |bc -l) )) 
     then
-            echo "Staking...."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.001tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id "crossfire" --node $node  -y
+            echo "Withdrawing rewards..."
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $validatorAddress --commission --from $keyring --gas 800000000 --gas-prices 0.1basetcro --chain-id="crossfire"  --node $node  -y
     fi
-    sleep 5s
+    if (( $(echo "$currentBalance > 1000" |bc -l) )) 
+    then
+            echo "Re-delegating rewards..."
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.001basetcro --from $keyring --gas 800000000 --gas-prices 0.1basetcro --chain-id="crossfire"  --node $node  -y
+    fi
+    sleep 4m
 done
