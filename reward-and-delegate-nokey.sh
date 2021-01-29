2f75f60a3bd6157773acd57c38c8925daaa1c150
abhinavbitra <77645385+abhinavbitra@users.noreply.github.com>
2021-01-28 21:55:29 -0600

    Update reward-and-delegate-nokey.sh

--- reward-and-delegate-nokey.sh
+++ reward-and-delegate-nokey.sh
@@ -27,8 +27,8 @@ do
     if (( $(echo "$currentBalance > 10000" |bc -l) )) 
     then
             echo "Re-delegating rewards..."
-            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.01tcro --from $cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
+            echo $keyPassword | ./chain-maind tx staking delegate $validatorAddress 0.01tcro --from cross-fire-testing --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
 
 fi
-    sleep 4m
+    sleep 4s
 done
